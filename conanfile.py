9ed004152e9c6267cf64d5427601ba1e163c1c9e
Illya Litkin <ilya.ltkn@gmail.com>
2026-05-24 22:22:46 +0000

    [CI] add build workflow

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,8 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+import re
 
 CONAN_TO_DEB_ARCH = {
     "armv8":   "arm64",
@@ -13,21 +16,46 @@ CONAN_TO_DEB_ARCH = {
 class openpressod(ConanFile):
     name = "openpressod"
     settings = "os", "arch", "compiler", "build_type"
+    options = { 
+        "with_clang_tools": [True, False]
+    }
+    default_options = { 
+        "with_clang_tools": False
+    }
+
+    def set_version(self):
+        if not self.version:
+            self.version = "0.0.0-unknown"
+
+    def validate(self):
+        check_min_cppstd(self, "23")
+        if not self.settings.os == "Linux":
+            raise ConanInvalidConfiguration("Only Linux is supported")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.30]")
+        self.test_requires("gtest/1.17.0")
 
     def requirements(self):
         self.requires("spdlog/[>=1.17.0 <2.0]")
         self.requires("toml11/[>=4.4.0]")
         self.requires("magic_enum/[>=0.9.7]")
         
-        self.requires("libopenpresso/1.0.0-rc2")
+        self.requires("libopenpresso/0.0.0-7-gce5e05c-feat-steam-temperature-controller")
         self.requires("openpresso_proto/1.0.0-rc1")
-
-    def build_requirements(self):
-        self.test_requires("gtest/1.17.0")
-
+        
     def generate(self):
+        major, minor, patch = self.__version_components()
         tc = CMakeToolchain(self)
+
+        if self.options.with_clang_tools:
+            self.__intall_clang_tools(tc)
+
         tc.cache_variables["CPACK_DEBIAN_PACKAGE_ARCHITECTURE"] = CONAN_TO_DEB_ARCH[str(self.settings.arch)]
+        tc.cache_variables["OPENPRESSOD_VERSION"] = self.version
+        tc.cache_variables["OPENPRESSOD_VERSION_MAJOR"] = str(major)
+        tc.cache_variables["OPENPRESSOD_VERSION_MINOR"] = str(minor)
+        tc.cache_variables["OPENPRESSOD_VERSION_PATCH"] = str(patch)
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
@@ -39,4 +67,23 @@ class openpressod(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+
+    def __version_components(self):
+        try:
+            pattern = r'(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)'
+            match = re.match(pattern, self.version)
+            return int(match.group("major")), int(match.group("minor")), int(match.group("patch"))
+        except Exception:
+            return 0, 0, 0
+
+    def __intall_clang_tools(self, cmtc):
+        try:
+            from conan.tools.system import PyEnv
+        except ImportError:
+            from conan.tools.system import PipEnv as PyEnv
+        venv = PyEnv(self)
+        venv.install(["clang-tidy", "clang-format"])
+        venv.generate()
+        bin_dir = getattr(venv, "bin_path", None) or getattr(venv, "bin_dir", None)
+        cmtc.cache_variables["CMAKE_PROGRAM_PATH"] = bin_dir
             
